5c8615a24edd6a5bdb00cb5ff9205fcce02ffcd1
pipeleon <oflg94@gmail.com>
2022-01-01 21:53:28 -0800

    Task 0 corrected case import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add
-answer = add(a, b)
-print("{} + {} = {}".format(a, b, answer))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    from add_0 import add
+    answer = add(a, b)
+    print("{} + {} = {}".format(a, b, answer))
