6b7d62c9341c3934f949b72c70afc38354c1622d
pipeleon <oflg94@gmail.com>
2022-01-18 13:25:11 -0800

    Task 0 with commets

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
+""" Task 0 Project 0x05 """
 #!/usr/bin/python3
 class Square():
-    """ This defines a class type Square
-    without atributes"""
+    """ This defines a class type Square without atributes"""
     pass
