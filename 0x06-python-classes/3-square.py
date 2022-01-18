21f9d0d1ad3115ceedb7e0f531d1f68fa9ca5b1a
pipeleon <oflg94@gmail.com>
2022-01-18 14:18:22 -0800

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 3 Project 0x06 Python"""
+
+
+class Square():
+    """ Class Square with argument size"""
+    def __init__(self, size=0):
+        """ Init method
+        Args:
+        param1 (size): Size of a Square"""
+        if type(size) != int:
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """ MEthod of calculation of Area"""
+        return self.__size * self.__size
