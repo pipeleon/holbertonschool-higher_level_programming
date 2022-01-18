622441b1527ee95763a987607559c48ff9e9df6d
pipeleon <oflg94@gmail.com>
2022-01-18 13:43:22 -0800

    Task 1 with comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""" Task 1 Project 0x06 Python"""
+
+
 class Square():
+    """ Define a class type Square with a size atribute """
     def __init__(self, size):
+        """ init method for Square class
+        Args:
+        param1 (size): Size of a Square"""
         self.__size = size
