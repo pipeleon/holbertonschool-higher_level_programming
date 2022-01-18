cc99b27f585ca6f8c55f0edc6c95a0c7ec05acdd
pipeleon <oflg94@gmail.com>
2022-01-18 07:57:48 -0800

    Task 101 corrected case no args

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,10 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        n = fct(args[0], args[1])
+        if not args:
+            n = fct()
+        else:
+            n = fct(args[0], args[1])
         return n
     except Exception as err:
         print("Exception:", err, file=sys.stderr)
