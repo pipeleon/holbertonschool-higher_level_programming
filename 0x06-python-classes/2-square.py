72b8772c86cee2524d3b09c0a34547c6eb78c190
pipeleon <oflg94@gmail.com>
2022-01-18 14:20:56 -0800

    Task 2 and 3 corrected

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,14 +2,16 @@
 """Task 2 Project 0x06 Python"""
 
 
-class Square():
+class Square:
     """ Class Square with argument size"""
+    pass
     def __init__(self, size=0):
         """ Init method
         Args:
         param1 (size): Size of a Square"""
         if type(size) != int:
             raise TypeError("Size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
