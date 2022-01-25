947a92f73dc700fc491b32893e0dc1fd5e73ba21
pipeleon <oflg94@gmail.com>
2022-01-24 22:25:19 -0500

    Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Task 1 Project 0x06 """
+
+
+class Rectangle():
+    """ Class Rectangle with argument size"""
+    def __init__(self, width=0, height=0):
+        """ Init method """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Width Method when called"""
+        return self.__width
+
+    @property
+    def height(self):
+        """ Height Method when called"""
+        return self.__height
+
+    @width.setter
+    def width(self, width):
+        """ Setter width"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        """ Setter height"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        """ Method of calculation of Area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Method of calculation of Perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (2 * self.__width) + (2 * self.__height)
