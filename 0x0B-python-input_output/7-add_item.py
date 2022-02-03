b05a8d283287c5e476cf2efa1040170b578e11c3
pipeleon <oflg94@gmail.com>
2022-02-02 22:00:59 -0500

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 7 Project 0x0B Python"""
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys, os.path
+
+
+if __name__ == "__main__":
+    if not os.path.exists("add_item.json"):
+        new = []
+        for i in range(1, len(sys.argv)):
+            new.append(sys.argv[i])
+        save_to_json_file(new, "add_item.json")
+    else:
+        new = load_from_json_file("add_item.json")
+        for i in range(1, len(sys.argv)):
+            new.append(sys.argv[i])
+        save_to_json_file(new, "add_item.json")
