7704fe43125accf37b1cb683d7e196125b1bf3c4
pipeleon <oflg94@gmail.com>
2022-02-02 21:25:18 -0500

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task 6 Project 0x0B Python"""
+import json
+
+
+def load_from_json_file(filename):
+    """Function that creates an Object from a “JSON file”"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
