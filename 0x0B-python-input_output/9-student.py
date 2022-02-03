7db8f8b0296233872d9104e13a033f355fa9287b
pipeleon <oflg94@gmail.com>
2022-02-02 22:51:36 -0500

    Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Task 9 Project 0x0B Python"""
+
+
+class Student():
+    """Class student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        return self.__dict__
