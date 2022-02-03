049e9c872d3a2f7b2ad9fdebc58e7deb3397db30
pipeleon <oflg94@gmail.com>
2022-02-02 20:14:20 -0500

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Task 0 Project 0x0B Python"""
+
+
+def read_file(filename=""):
+    """"Function that openand read a file to print its content"""
+    with open(filename) as f:
+        print(f.read())
