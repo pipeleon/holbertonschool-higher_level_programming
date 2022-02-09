692483f266c0eaf7a0677cf1dc605a196950d2c5
pipeleon <oflg94@gmail.com>
2022-02-08 23:49:13 -0500

    First tests case for Base

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,6 +10,15 @@ class TestsRectangle(unittest.TestCase):
         b1 = Rectangle(10, 2)
         self.assertEqual(b1.width, 10)
         b2 = Rectangle(2, 10)
-        self.assertEqual(b2.width, 2)
+        self.assertEqual(b2.height, 10)
         b3 = Rectangle(10, 2, 0, 0, 89)
         self.assertEqual(b3.id, 89)
+        self.assertEqual(b3.x, 0)
+        self.assertEqual(b3.y, 0)
+        b1.x = 5
+        self.assertEqual(b1.x, 5)
+        b2.y = 9
+        self.assertEqual(b2.y, 9)
+
+    def test_valuesError(self):
+        self.assertRaises(TypeError, Rectangle, (10, 2.8))
