af54f8cdf69476a073f08730a4bab4b7080e27be
pipeleon <oflg94@gmail.com>
2022-02-08 19:57:32 -0500

    First tests case for Base

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unit Tests for class Base"""
+import unittest
+from models.base import Base
+
+
+class TestsBase(unittest.TestCase):
+
+    def test_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base(2)
+        self.assertEqual(b2.id, 2)
+        b3 = Base(89)
+        self.assertEqual(b3.id, 89)
