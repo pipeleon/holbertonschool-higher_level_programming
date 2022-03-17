ed4065580180143987d09039bdd441b396d63a0e
pipeleon <oflg94@gmail.com>
2022-03-17 15:52:35 -0700

    Task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+--   script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_shows.title, tv_genres.name FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, tv_genres.name ASC
