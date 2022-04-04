6818182bee7ea12703cdc39560040fc07fa053d2
pipeleon <oflg94@gmail.com>
2022-04-04 16:47:23 -0500

    Task 2 case multiple arguments corrected

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 
-if (process.argv[2]) {
+if (process.argv[3]) {
+  console.log('Arguments found');
+} else if (process.argv[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
