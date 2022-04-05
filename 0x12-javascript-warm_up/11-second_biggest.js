008a71f33f3524f7371acb3044fb0f6f06b31a97
pipeleon <oflg94@gmail.com>
2022-04-04 22:00:00 -0500

    Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+
+if (process.argv[3]) {
+  const list = [];
+  for (let i = 2; process.argv[i]; i++) {
+    list.push(parseInt(process.argv[i]));
+  }
+  list.sort();
+  list.reverse();
+  console.log(list[1]);
+} else {
+  console.log(0);
+}
