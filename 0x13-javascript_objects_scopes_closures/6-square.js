529de6d95ad66aad4a1b58cf18befea52e68fab4
pipeleon <oflg94@gmail.com>
2022-04-07 14:27:24 -0500

    Task 6 correction on calling Rectangle for extends

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    if (this.width && this.height) {
-      let row = 'X';
-      for (let i = 1; i < this.width; i++) {
-        row = row + 'X';
-      }
-      for (let j = 0; j < this.height; j++) {
-        console.log(row);
-      }
-    }
-  }
-
-  rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
