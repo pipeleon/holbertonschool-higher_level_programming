8bd58afaef054ce4d1cf8a98b58d87472bd4cb01
pipeleon <oflg94@gmail.com>
2022-04-07 18:22:06 -0500

    Task 102 change to a SYNCHRONOUS funtion for fs

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,29 +4,14 @@ const process = require('process');
 const nameFileA = process.argv[2];
 const nameFileB = process.argv[3];
 const nameFileC = process.argv[4];
+let content;
 
-const fs = require('fs')
+const fs = require('fs');
 
-fs.readFile(nameFileA, 'utf8' , (err, data1) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  fs.writeFile(nameFileC, data1, function (err) {
-    if (err) throw err;
-  });
-})
+content = fs.readFileSync(nameFileA, 'utf8');
+content = content + '\n';
+content = content + fs.readFileSync(nameFileB, 'utf8');
 
-
-fs.readFile(nameFileB, 'utf8' , (err, data2) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  fs.appendFile(nameFileC, '\n', function (err) {
-    if (err) throw err;
-  });
-  fs.appendFile(nameFileC, data2, function (err) {
-    if (err) throw err;
-  });
-})
+fs.writeFile(nameFileC, content, function (err) {
+  if (err) throw err;
+});
