6d21f1328ccb84f25c1c631a9bdb430c55aa1706
pipeleon <oflg94@gmail.com>
2022-04-07 13:54:52 -0500

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      let row = 'X';
+      for (let i = 1; i < this.width; i++) {
+        row = row + 'X';
+      }
+      for (let j = 0; j < this.height; j++) {
+        console.log(row);
+      }
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
