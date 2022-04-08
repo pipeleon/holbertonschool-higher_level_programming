42e2838e138730684820787d987f1c8625d94b35
pipeleon <oflg94@gmail.com>
2022-04-08 09:15:41 -0500

    Task 6 case when double

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,10 +9,10 @@ module.exports = class Square extends Rectangle {
 
   charPrint (c = 'X') {
     let row = c;
-    for (let i = 1; i < this.size; i++) {
+    for (let i = 1; i < this.width; i++) {
       row = row + c;
     }
-    for (let j = 0; j < this.size; j++) {
+    for (let j = 0; j < this.height; j++) {
       console.log(row);
     }
   }
