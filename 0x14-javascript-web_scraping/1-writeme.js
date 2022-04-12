0323ba6697fc1dafc19ba984be6f841d3bb48f9e
pipeleon <oflg94@gmail.com>
2022-04-11 22:21:39 -0500

    Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+const fs = require('fs');
+
+fs.writeFileSync(filePath, stringToWrite, function (err) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
