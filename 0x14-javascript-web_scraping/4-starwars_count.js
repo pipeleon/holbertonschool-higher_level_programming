e82f84c41f0e8ab93d583bc42bde942f450991b6
pipeleon <oflg94@gmail.com>
2022-04-12 16:35:50 -0500

    Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let count = 0;
+    const movies = JSON.parse(body).results;
+    for (let j = 0; movies[j]; j++) {
+      for (let i = 0; movies[j].characters[i]; i++) {
+        if (movies[j].characters[i] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
