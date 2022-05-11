c00b511124441caf0d8549fe94b0c1695d472a3e
pipeleon <oflg94@gmail.com>
2022-05-10 22:11:25 -0700

    Task 100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Task 8: query by filter"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """Query for the first state"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+    new_state = State(name='California', cities=City(name='San Francisco'))
+    session.add(new_state)
+    session.commit()
