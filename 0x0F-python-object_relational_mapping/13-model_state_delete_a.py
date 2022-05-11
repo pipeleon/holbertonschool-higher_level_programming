b5d7c4e3a6107e7a1d94f76855a8b1fae3f6c112
pipeleon <oflg94@gmail.com>
2022-05-10 20:55:25 -0700

    Tasl 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Task 13: query by filter"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """Query for the first state"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).\
+        order_by(State.id).filter(State.name.like('%a%'))
+
+    for i in query:
+        session.delete(i)
+    session.commit()
