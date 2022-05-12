702b7f2ed7f243c90c205bd79bebc6298c6065ef
pipeleon <oflg94@gmail.com>
2022-05-12 12:12:54 -0500

    Task 100 test backref

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,5 +11,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('State.id', ondelete="CASCADE"),
-                      nullable=False)
+    state_id = Column(Integer, ForeignKey('State.id'), nullable=False)
