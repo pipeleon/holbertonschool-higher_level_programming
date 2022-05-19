f9710f9f69aaa69ede4c2df7b67f662e3be3ce99
pipeleon <oflg94@gmail.com>
2022-05-19 13:51:17 -0500

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 2 0x11. Python - Network #1"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    """Main Function"""
+    url = sys.argv[1]
+    data = {}
+    data['email'] = sys.argv[2]
+    url_values = urllib.parse.urlencode(data).encode('ascii')
+    req = urllib.request.Request(url, url_values)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html)
