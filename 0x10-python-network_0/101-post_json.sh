f4bf28eef4a59702524a7f54b5a921cf64ec4ffe
pipeleon <oflg94@gmail.com>
2022-05-19 02:43:53 +0000

    Task 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that displays the result of sending a header specific
-curl -s -X POST "$1" -H 'Content-Type: application/json' -d "$2"
+curl -s -X POST "$1" -H 'Content-Type: application/json' -d @"$2"
