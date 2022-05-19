1311904dc2f606984781be3159a1cbc35917def3
pipeleon <oflg94@gmail.com>
2022-05-19 00:56:41 +0000

    Task 0 with arguments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that displays the size of the body of the response
-curl -sI 0.0.0.0:5000 | grep 'Content-Length' | awk '{print $2}'
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
