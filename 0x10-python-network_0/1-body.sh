641b7da0e349c6827b9625133d1da8e93e5dbd67
pipeleon <oflg94@gmail.com>
2022-05-19 01:23:07 +0000

    Task 1 without condition

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # Script that displays the body of the response
-response=$(curl --write-out '%{http_code}' --silent --output /dev/null -L "$1")
-OK="200"
-if [[ "$response" -eq "$OK" ]]; then
-    curl -L "$1"
-fi
+curl -sL "$1"
+
