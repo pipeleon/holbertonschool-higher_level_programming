18d20690ed30189f35582717eee8fd7945b7dbc0
pipeleon <oflg94@gmail.com>
2022-05-18 22:57:55 -0500

    Task 6 style

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ function that finds a peak in a list of unsorted integers"""
 
+
 def find_peak(list_of_integers):
     """Fucntion"""
     if len(list_of_integers) == 0:
         return None
 
     list_of_integers.sort()
-    
+
     return list_of_integers[-1]
