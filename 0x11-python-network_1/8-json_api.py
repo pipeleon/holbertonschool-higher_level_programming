8003aee337007458c79ae9cb0d99605467415daf
pipeleon <oflg94@gmail.com>
2022-05-20 19:06:25 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 8 0x11. Python - Network #1"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """Main Function"""
+    if len(sys.argv) > 0:
+        q = sys.argv[1]
+    else:
+        q = ""
+    params = {}
+    params['q'] = q
+    r = requests.post("http://0.0.0.0:5000/search_user", data=params)
+
+    if len(r.json().keys()) == 0:
+        print("No result")
+    else:
+        print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
